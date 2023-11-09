65f3acce8c0146518bfaf8c664c1acee07976c22
dima <dima>
2023-11-09 19:12:23 +0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,10 @@ class App extends React.Component {
   switchStateTask(index: number) {
 
 
-    this.setState(() => {
+
+
+
+    this.setState((state) => {
       return {
         tasks: this.state.tasks.map((el, i) => {
           if (index === i) {
