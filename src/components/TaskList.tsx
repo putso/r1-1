01d7506c945bbd273d1870f926f9a0da36862219
dima <dima>
2023-11-11 03:49:47 +0400

    clear comments

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import Task from './Task';
 import { iTask, taskHandlers } from '../type';
-export default class TaskList extends React.Component<{ taskHandlers: taskHandlers; tasks: iTask[] }, object> {
-  state: Readonly<{
-    tasks: iTask[];
-  }> = {
-    tasks: [],
-  };
-  render() {
-    const { taskHandlers, tasks } = this.props;
-    return (
-      <ul className="todo-list">
-        {tasks.map((task) => (
-          <Task key={task.created} data={task} taskHandlers={taskHandlers} />
-        ))}
-      </ul>
-    );
-  }
+export default function TaskList({ taskHandlers, tasks }: { taskHandlers: taskHandlers; tasks: iTask[] }) {
+  return (
+    <ul className="todo-list">
+      {tasks.map((task) => (
+        <Task key={task.created} data={task} taskHandlers={taskHandlers} />
+      ))}
+    </ul>
+  );
 }
--- src/components/TasksFilter.tsx
+++ src/components/TasksFilter.tsx
@@ -1,32 +1,5 @@
 import React from 'react';
 import { filter } from '../type';
-
-// export default class TasksFilter extends React.Component<
-//   {
-//     setFilter: (filter: filter) => void;
-//     filter: filter;
-//   },
-//   object
-// > {
-//   render() {
-//     const { filter, setFilter } = this.props;
-//     const filters: Capitalize<filter>[] = ['Active', 'All', 'Completed'];
-//     return (
-//       <ul className="filters">
-//         {filters.map((value, i) => (
-//           <li key={i}>
-//             <button
-//               className={value.toLocaleLowerCase() == filter ? 'selected' : ''}
-//               onClick={() => setFilter(value.toLocaleLowerCase() as filter)}
-//             >
-//               {value}
-//             </button>
-//           </li>
-//         ))}
-//       </ul>
-//     );
-//   }
-// }
 const filters: Capitalize<filter>[] = ['Active', 'All', 'Completed'];
 export default function TasksFilter({ filter, setFilter }: { setFilter: (filter: filter) => void; filter: filter }) {
   return (
